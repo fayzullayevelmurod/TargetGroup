9b13a1a83c48ece2e9f8868d800f8cc8e6b0d063
BOUNTY707 <mehrobakooo@gmail.com>
2024-01-31 00:28:35 +0500

    header, home, calculator, portfolio, deyarli tugadi

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// Header scroll 
+window.addEventListener('scroll', function () {
+    document.querySelector('.header').classList.toggle('header-fixed', window.scrollY > 0);
+});
+
+
+// Calc project tab active
+var calcProject = new Swiper(".calcProject", {
+    navigation: {
+        nextEl: ".calc-project-next",
+    },
+    speed: 1000,
+    allowTouchMove: false,
+    followFinger: false,
+});
+
+// Calc project tab active
+document.addEventListener('DOMContentLoaded', function () {
+    let tabs = document.querySelectorAll('.calc-project__tab');
+    let nextButton = document.querySelector('.calc-item__btn');
+
+    tabs.forEach(tab => tab.addEventListener('click', () => {
+        tab.classList.toggle('active');
+        nextButton.classList.toggle('active', tab.classList.contains('active'));
+    }));
+});
+
+
+// Calc project box active
+document.addEventListener('DOMContentLoaded', () => {
+    const boxes = document.querySelectorAll('.calc-project__box');
+    const nextButton2 = document.querySelector('.calc-item__btn2');
+
+    boxes.forEach(box => box.addEventListener('click', () => {
+        box.classList.toggle('active');
+        nextButton2.classList.toggle('active', box.classList.contains('active'));
+    }));
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let hiddenItems = document.querySelectorAll('.item-none');
+    let moreButton = document.querySelectorAll('.portfolio-more');
+    let removeButton = document.querySelectorAll('.portfolio-remove');
+
+    moreButton.addEventListener('click', function () {
+        hiddenItems.forEach(item => item.style.display = 'flex');
+        moreButton.style.display = 'none';
+        removeButton.style.display = 'block';
+    });
+
+    removeButton.addEventListener('click', function () {
+        hiddenItems.forEach(item => item.style.display = 'none');
+        removeButton.style.display = 'none';
+        moreButton.style.display = 'block';
+    });
+
+    document.querySelectorAll('.portfolio-item').forEach(item => {
+        item.addEventListener('click', function () {
+            console.log('Portfolio item clicked:', item.querySelector('.portfolio-item__title').textContent);
+        });
+    });
+});
+
