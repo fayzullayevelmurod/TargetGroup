37da1fc1a33332e6d4f2b52a8be712554ed9dc41
BOUNTY707 <mehrobakooo@gmail.com>
2024-02-06 20:53:23 +0500

    pravkalar tug'irlandi

--- js/main.js
+++ js/main.js
@@ -405,7 +405,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function applyMask(input) {
         let maskOptions = {
-            mask: '+375(00)000-00-00',
+            mask: '+8 (000) 000-00-00',
             lazy: false
         };
 
